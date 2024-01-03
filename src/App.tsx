e2a714ca247362e06dd8f630070c32c0c86e9e8a
clean <1518136109@qq.com>
2024-01-03 23:10:01 +0800

    chore: test tree-model

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,61 @@ import "./App.css";
 import NbCanvas from "./components/NbCanvas";
 import EchartDemo from "./demo/echarts";
 import FormTest from "./components/FormTest";
-import TreeNode from "./utils/my_tree";
+import MyTreeModel from "tree-model";
 function App() {
-	type nodeTest = {
+	type DirNode = {
 		// 文件夹名称
 		dirName: string;
 		// 文件夹绝对路径
 		dirPath: string;
 		// 包含的文件夹
-		childrenDir?: nodeTest[];
+		childrenDir?: DirNode[];
 		// 文件夹下除文件夹外的文件名
 		fileNames?: string[];
 		// 父文件夹
-		parent?: nodeTest;
+		parent?: DirNode[];
 	};
-
-	const DirTreeNode = TreeNode<nodeTest>;
-
-	const root = new DirTreeNode({
+	const tree = new MyTreeModel({ childrenPropertyName: "childrenDir" });
+	const dirObj: DirNode = {
 		dirName: "root",
-		dirPath: "",
+		dirPath: "/root",
+		fileNames: [],
+		childrenDir: [
+			{
+				dirName: "child_1",
+				dirPath: "/root/child_1",
+				fileNames: [],
+				childrenDir: [
+					{
+						dirName: "child_1_1",
+						dirPath: "/root/child_1/child_1_1",
+					},
+				],
+			},
+			{
+				dirName: "child_2",
+				dirPath: "/root/child_2",
+				fileNames: [],
+			},
+		],
+	};
+	const root = tree.parse(dirObj);
+	// const child = tree.parse({
+	// 	id: "child_1",
+	// });
+	// root.addChild(child);
+	// root.addChild(child);
+
+	console.log("root", root);
+	// console.log(child.getPath());
+	root.walk(node => {
+		console.log(node);
+		if (node.hasChildren()) {
+			node.model.fileNames.push(...["fil1", "fil2", "file3"]);
+		}
+		return true;
 	});
 
-	const child = new DirTreeNode({
-		dirName: "root",
-		dirPath: "",
-	});
-
-	root.addChild(child);
-
-	console.log(root);
 	return (
 		// <div className="App">
 		// </div>
